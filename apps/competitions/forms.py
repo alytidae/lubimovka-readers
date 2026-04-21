798fd599d63298bea893441fde40c28c2d6c4cbc
Igor Botukhov <igor.botukhov@gmail.com>
2026-04-21 20:11:22 +0200

    Added forms for competitions

---
+++ apps/competitions/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from .models import Competition
+
+class CompetitionCreationForm(forms.ModelForm):
+    class Meta:
+        model = Competition
+        fields = (
+            'title', 
+            'date', 
+            'google_sheet_url', 
+            'play_title_sheet_column_name', 
+            'play_link_sheet_column_name'
+        )
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            'google_sheet_url': forms.URLInput(attrs={
+                'placeholder': 'https://docs.google.com/spreadsheets/d/...'
+            }),
+        }
+
+class CompetitionChangeForm(forms.ModelForm):
+    class Meta:
+        model = Competition
+        fields = (
+            'title', 
+            'date', 
+            'status',
+            'are_phase1_reviews_visible',
+            'are_phase2_reviews_visible',
+            'google_sheet_url', 
+            'play_title_sheet_column_name', 
+            'play_link_sheet_column_name'
+        )
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+        }
