8c087391ca1d62abc48e73bdc570f01e4bece6b7
Igor Botukhov <igor.botukhov@gmail.com>
2026-04-21 20:07:18 +0200

    Finished urls and views for competitions

--- apps/competitions/urls.py
+++ apps/competitions/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'competition'
+app_name = 'competitions'
 
 urlpatterns = [
-    path('create/', views.UserCreateView.as_view(), name='create'),
-    path('update/<int:pk>/', views.UserUpdateView.as_view(), name='update'),
+    path('', views.CompetitionListView.as_view(), name='list'),
+    path('create/', views.CompetitionCreateView.as_view(), name='create'),
+    path('<slug:slug>/update/', views.CompetitionUpdateView.as_view(), name='update'),
+    path('<slug:slug>/', views.CompetitionDetailView.as_view(), name='detail'),
 ]
--- apps/competitions/views.py
+++ apps/competitions/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, ListView, DetailView
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from .models import Competition
+from .forms import CompetitionCreationForm, CompetitionChangeForm 
 
-# Create your views here.
+class CompetitionCreateView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, CreateView):
+    model = Competition
+    template_name = "competition_create.html"
+    form_class = CompetitionCreationForm
+    success_message = "\u2705 %(title) was created successfully" 
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+class CompetitionUpdateView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, UpdateView):
+    model = Competition
+    template_name = "competition_update.html"
+    form_class = CompetitionChangeForm
+    success_message = "\u2705 %(title) was updated successfully"
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+
+    def test_func(self):
+        user = self.request.user
+        if user.is_superuser:
+            return True
+            
+        return user.competition_roles.filter(
+            competition=self.get_object(), 
+            role='admin'
+        ).exists()
+
+class CompetitionListView(LoginRequiredMixin, ListView):
+    model = Competition
+    template_name = "competition_list.html"
+    context_object_name = "competitions"
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return Competition.objects.all()
+            
+        return Competition.objects.filter(roles__user=user).distinct()
+
+class CompetitionDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+    model = Competition
+    template_name = "competition_detail.html"
+    context_object_name = "competition"
+
+    def test_func(self):
+        user = self.request.user
+        if user.is_superuser:
+            return True
+            
+        return user.competition_roles.filter(
+            competition=self.get_object()
+        ).exists()
