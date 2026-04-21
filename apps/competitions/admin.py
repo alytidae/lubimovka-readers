196d89fdb87fbe4ee032b0ac280ed351c72beff9
Igor Botukhov <igor.botukhov@gmail.com>
2026-04-21 20:35:52 +0200

    Fixed slug and added admin tables

--- apps/competitions/admin.py
+++ apps/competitions/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Competition, CompetitionRole
 
-# Register your models here.
+
+@admin.register(Competition)
+class CompetitionAdmin(admin.ModelAdmin):
+    list_display = ("title", "date", "status")
+    search_fields = ("title",)
+    ordering = ("-date",)
+
+
+@admin.register(CompetitionRole)
+class CompetitionRoleAdmin(admin.ModelAdmin):
+    list_display = ("user", "competition", "role")
+    list_filter = ("competition", "role")
+    search_fields = ("user__email", "competition__title")
